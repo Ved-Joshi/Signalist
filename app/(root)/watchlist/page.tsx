30372c83ba8d6cb39727c95eb5871351d17a3617
Ved-Joshi <behumble1907@gmail.com>
2025-11-19 15:18:27 -0800

    add watchlist

--- app/(root)/watchlist/page.tsx
+++ app/(root)/watchlist/page.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import {Star} from "lucide-react"
+import {searchStocks} from "@/lib/actions/finnhub.actions";
+import SearchCommand from "@/components/SearchCommand";
+import {WatchlistTable} from "@/components/WatchlistTable";
+import {getWatchlistWithData} from "@/lib/actions/watchlist.actions";
 
-const Watchlist = () => {
+const Watchlist = async () => {
+    const watchlist = await getWatchlistWithData();
+    const initialStocks = await searchStocks();
+
+    if (watchlist.length == 0) {
+        return (
+            <section className={"flex watchlist-empty-container"}>
+                <div className={"watchlist-empty"}>
+                    <Star className={"watchlist-star"}/>
+                    <h2 className={"empty-title"}>Your Watchlist is Empty</h2>
+                    <p className={"empty-description"}>
+                        Start building your watchlist by searching for stocks and clicking the star icon to add them.
+                    </p>
+                </div>
+                <SearchCommand initialStocks={initialStocks}/>
+            </section>
+        )
+    }
     return (
-        <div>Watchlist</div>
+        <section className={"watchlist-container"}>
+            <div className={"flex flex-col gap-6 lg:col-span-3"}>
+                <div className={"flex items-center justify-between"}>
+                    <h2 className={"watchlist-title"}>Watchlist</h2>
+                    <SearchCommand initialStocks={initialStocks}/>
+                </div>
+                <WatchlistTable watchlist={watchlist} />
+            </div>
+        </section>
     )
 }
 export default Watchlist
